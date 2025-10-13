85443873c13d7ceae452fba741a45efb60aadd00
gooya3000 <withthdud0923@gmail.com>
2025-10-14 00:30:33 +0900

    Initial commit

---
+++ src/main/java/com/example/aprbackendassignment/domain/Friends.java
@@ -0,0 +1,12 @@
+package com.example.aprbackendassignment.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Friends {
+    @Id
+    private Long id;
+}
---
+++ src/main/java/com/example/aprbackendassignment/domain/User.java
@@ -0,0 +1,12 @@
+package com.example.aprbackendassignment.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+    @Id
+    private Long id;
+}
