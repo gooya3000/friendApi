1c70f3b9b74307a4813e01acb1267a928b84dbbb
gooya3000 <withthdud0923@gmail.com>
2025-10-14 23:40:11 +0900

    Revert "엔티티명 수정"
    This reverts commit e40a5d993bf8c0ab6f77335402951e7ba57997b3.

--- src/main/java/com/example/aprbackendassignment/controller/UserController.java
+++ src/main/java/com/example/aprbackendassignment/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.aprbackendassignment.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api-v1/user")
-public class UsersController { }
+public class UserController { }
--- src/main/java/com/example/aprbackendassignment/domain/User.java
+++ src/main/java/com/example/aprbackendassignment/domain/User.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Users {
+public class User {
     @Id
     private Long id;
 }
