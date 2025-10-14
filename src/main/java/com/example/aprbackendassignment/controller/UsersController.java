e40a5d993bf8c0ab6f77335402951e7ba57997b3
gooya3000 <withthdud0923@gmail.com>
2025-10-14 23:33:59 +0900

    엔티티명 수정

--- src/main/java/com/example/aprbackendassignment/controller/UsersController.java
+++ src/main/java/com/example/aprbackendassignment/controller/UsersController.java
@@ -1,9 +1,7 @@
 package com.example.aprbackendassignment.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api-v1/user")
-public class UserController { }
+public class UsersController { }
