e29952019dc228664af8e92ba092ce30286162ea
gooya3000 <withthdud0923@gmail.com>
2025-10-20 00:36:30 +0900

    불필요 디렉토리 삭제

--- src/main/java/com/example/aprbackendassignment/controller/user/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.example.aprbackendassignment.controller.user;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/user")
-public class UserController { }
